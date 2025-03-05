d4577971b4b53238ca5d31385704b66f25f0fbd6
Hamza-Belcaid <Belcaidhamza43215@gmail.com>
2025-03-05 11:42:39 +0000

    Fixed an error in the script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo -e "Choose your AUR helper : ( yay | paru )"
-read $aur
+read aur
 
-if [[ $aur != "yay" && $aur != paru ]]; then
+if [[ $aur != "yay" && $aur != "paru" ]]; then
     echo "Invalid option! \nPlease restart the script and enter a valid option (yay or paru)"
 fi
 
-$aur -Syu --noconfirm plasma kate konsole dolphin ark gwenview partitionmanager filelight libreworlf-bin vlc qbittorrent localsend onlyoffice-bin steam heroic-games-launcher discord mangohud goverlay protonup-qt fuse2 fastfetch ventoy-bin keepassxc github-cli
+$aur -Syu --noconfirm plasma kate konsole dolphin ark gwenview partitionmanager filelight libreworlf-bin vlc qbittorrent localsend onlyoffice-bin steam heroic-games-launcher discord mangohud goverlay protonup-qt fuse2 fastfetch ventoy-bin keepassxc git github-cli

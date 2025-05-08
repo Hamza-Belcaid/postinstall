6d026f0877f796f904119705073a837d44d2c931
Hamza-Belcaid <Belcaidhamza43215@gmail.com>
2025-05-08 15:09:05 +0100

    Repo Restructure

---
+++ kde.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+paru -Syu --noconfirm --needed plasma kate konsole dolphin ark gwenview partitionmanager filelight
+
+
---
+++ programs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+paru -Syu --noconfirm --needed libreworlf-bin vlc qbittorrent localsend onlyoffice-bin steamheroic-games-launcher-bin discord mangohud goverlay protonup-qt fuse2 fastfetch ventoy-bin keepassxc git github-cli
+

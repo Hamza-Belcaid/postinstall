e284a35f96839691993f378fe8f820bcca06155a
Hamza-Belcaid <Belcaidhamza43215@gmail.com>
2025-05-08 16:06:55 +0100

    Added more programs

--- programs.sh
+++ programs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-paru -Syu --noconfirm --needed libreworlf-bin vlc qbittorrent localsend onlyoffice-bin steamheroic-games-launcher-bin discord mangohud goverlay protonup-qt fuse2 fastfetch ventoy-bin keepassxc git github-cli
-
+paru -Syu --noconfirm --needed libreworlf-bin vlc qbittorrent localsend-bin wifi-hotspot onlyoffice-bin discord protonup-qt fuse2 fastfetch ventoy-bin keepassxc git github-cli steam heroic-games-launcher-bin rpcs3-bin prismlauncher mangohud mangojuice obs-studio kleopatra
